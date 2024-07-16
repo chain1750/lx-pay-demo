ae1fd607028f4f8a6600593ce43ad25d1217aa9a
chenhaizhuang <chinesechain@163.com>
2024-07-17 00:32:58 +0800

    init

---
+++ src/main/java/com/lx/pay/Application.java
@@ -0,0 +1,22 @@
+package com.lx.pay;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author chenhaizhuang
+ */
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("启动成功");
+    }
+}
